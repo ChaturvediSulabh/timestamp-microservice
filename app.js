b56dfeda20841afbcc1a3b3b5cc214c6c0107e1b
ChaturvediSulabh <sulabhchaturvedi08@gmail.com>
2018-05-07 16:06:54 +0000

    updated code to resolve the bug highted at FCC forum - https://forum.freecodecamp.org/t/timestamp-microservice-feedback/46724/8

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var app = express();
-var url = require('url');
-var path = require('path')
+const express = require('express');
+const app = express();
+const url = require('url');
+const path = require('path')
 app.get('', function(req, res){
     res.sendfile('default.html', { root: __dirname + "/client"} );
 });
@@ -9,14 +9,16 @@ app.get('/*',function(req,res){
   const data = url.parse(req.url,true);
   var query = data.path;
   var myObj = {};
+  const isEpoch = query.replace("/","").search(/^\d+/g);
   const isFound = query.search(/[a-zA-Z0-9,%]+/g)
-  if(query.length === 11){
+  if(isEpoch != -1){
     query = query.replace("/","");
     myObj.unix = parseInt(query);
     var u_d = parseInt(query) * 1000;
     u_d = new Date(u_d);
     myObj.natural = u_d.toDateString();
   }else if (isFound != -1) {
+    console.log("here")
     var dStr = query.replace(/%20/g," ");
     n_d = new Date(dStr);
     n_d = n_d.toDateString();
