faff3317ffaef3c88e25fb82da7ac68420ef3a08
ChaturvediSulabh <sulabhchaturvedi08@gmail.com>
2018-05-07 11:40:40 +0000

    Resolved unix get issue - used regex

--- app.js
+++ app.js
@@ -6,13 +6,15 @@ app.get('', function(req, res){
     res.sendfile('default.html', { root: __dirname + "/client"} );
 });
 app.get('/*',function(req,res){
-  const data = url.parse(req.url,true);
+   data = url.parse(req.url,true);
   var query = data.path;
   var myObj = {};
-  const isFound = query.search(/[a-zA-Z0-9,%]+/g)
-  if(query.length === 10){
-    myObj.unix = parseInt(query);
-    var u_d = parseInt(query) * 1000;
+  var isunix = query.match(/[0-9]+/g);
+  isunix = isunix.join('');
+  var isFound = query.search(/[a-zA-Z0-9,%]+/g);
+  if(isunix.length === 10){
+    myObj.unix = parseInt(isunix);
+    var u_d = parseInt(isunix) * 1000;
     u_d = new Date(u_d);
     myObj.natural = u_d.toDateString();
   }else if (isFound != -1) {
@@ -27,9 +29,8 @@ app.get('/*',function(req,res){
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify(myObj));
    }else {
-           res.writeHead(404);
-           res.end();
+      res.writeHead(404);
+      res.end();
    }
 });
 app.listen(process.env.PORT || 8080);
-
